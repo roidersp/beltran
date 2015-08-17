79404324c3b3d8d017dd8d70fa8cd5dcab10dbaa
Rodrigo Luis Santander <roidersp@gmail.com>
2015-08-16 22:25:25 -0500

    9y8p

---
+++ application/views/main.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<link rel="stylesheet" href="css/estilos.css" />
+		<link rel="stylesheet" href="css/normalize.css"/>
+	</head>
+	<body> 
+		<div class="main_container">
+			<div class="contain">
+				<div class="login_contain">
+					<div class="login">
+						<
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
