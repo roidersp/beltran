3d937be28ff36814eaa61b17c701fb2327a17e5e
Rodrigo Luis Santander <roidersp@gmail.com>
2015-08-28 14:30:20 -0500

    fdgdfg

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -21,7 +21,7 @@ class Admin extends CI_Controller {
 	 
 	 public function __construct() {
         parent::__construct();
-        $this->load->helper(array('url'));
+        $this->load->helper(array('form', 'url'));
         $this->load->model(array('usuarios_model'));
 		$this->load->model(array('expedientes_model'));
 		$this->load->model(array('acuerdo_model'));
@@ -120,33 +120,55 @@ class Admin extends CI_Controller {
 	
 	public function uploadimages()
 	{
-		$config['upload_path']   =   "acuerdos/";
-        $config['allowed_types'] =   "gif|jpg|jpeg|png"; 
-        $config['max_size']      =   "5000";
-        $config['max_width']     =   "1907";
-        $config['max_height']    =   "1280";
- 
-        $this->load->library('upload',$config);
- 
-        if(!$this->upload->do_upload())
-        {
-	   		echo "cytvub";
-	   		echo $this->upload->display_errors();
-	   	}else{
-           $finfo=$this->upload->data();
-           $data['uploadInfo'] = $finfo;
- 
- 
- 
-           // You can view content of the $finfo with the code block below
- 
-           echo '<pre>';
- 
-           print_r($finfo);
- 
-           echo '</pre>';
- 
-       }
+
+       
+	       // Detect form submission.
+	       echo("test");
+	       var_dump($_POST);
+	       
+	       var_dump($this->input->post('submit'));
+	       
+	        if($this->input->post('submit')){
+	        
+	            $path = '/beltran/acuerdos/';
+	            $this->load->library('upload');
+	            /**
+	             * Refer to https://ellislab.com/codeigniter/user-guide/libraries/file_uploading.html 
+	             * for full argument documentation.
+	             *
+	             */
+	             
+	            // Define file rules
+	            $this->upload->initialize(array(
+	                "upload_path"       =>  $path,
+	                "allowed_types"     =>  "gif|jpg|png",
+	                "max_size"          =>  '100',
+	                "max_width"         =>  '1024',
+	                "max_height"        =>  '768'
+	            ));
+	            
+	            if($this->upload->do_multi_upload("uploadfile")){
+	                
+	                $data['upload_data'] = $this->upload->get_multi_upload_data();
+	                
+	                echo '<p class = "bg-success">' . count($data['upload_data']) . 'File(s) successfully uploaded.</p>';
+	                
+	            } else {    
+	                // Output the errors
+	                $errors = array('error' => $this->upload->display_errors('<p class = "bg-danger">', '</p>'));               
+	            
+	                foreach($errors as $k => $error){
+	                    echo $error;
+	                }
+	                
+	            }
+	            
+	        } else {
+	            echo '<p class = "bg-danger">An error occured, please try again later.</p>';
+	            
+	        }
+	        // Exit to avoid further execution
+	        exit();
 
 	}
 	
--- application/views/admin/addAcuerdo.php
+++ application/views/admin/addAcuerdo.php
@@ -13,12 +13,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<link rel="stylesheet" href="/beltran/css/admin.css" />
 		<link rel="stylesheet" href="/beltran/css/normalize.css"/>
 		<script type="text/javascript" src="/beltran/js/jquery.min.js"></script>
-		<script type="text/javascript">
-			$(document).on("change","#inputimages",function(event){
-				files = event.target.files;
-				console.log(files);
-			});
-		</script>
+		<script type="text/javascript" src="/beltran/js/jquery.ajaxfileupload.js"></script>
+		
 	</head>
 	<body> 
 		<div class="main_container">
@@ -49,11 +45,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 									<input type="text" name="tipo" />
 									
 									
-									
+									<input type="submit" value="Guardar"/>
 								</form>
 								
-								<form id="addimage">
-									<input type="file" id="inputimages" name="images[]" accept="image/*" draggable multiple="multiple" />
+								
+								<form action="http://localhost/beltran/index.php/admin/uploadimages" accept-charset="utf-8" id="addimage">
+
+									<input type="file" id="inputimages" name="images"  />
+									<input type="hidden" value="test">
+									<input type="submit" value="test" />
 								</form>	
 								
 								
@@ -61,7 +61,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 									
 								</div>
 																		
-								<input type="submit" value="Guardar"/>
+								
 													
 							</div>
 						</div>
@@ -71,5 +71,93 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 			
 		</div>
+		<script type="text/javascript">
+			
+			
+			/*var options = {
+                beforeSend: function(){
+                    // Replace this with your loading gif image
+                    $("#images_conrainer").html('subiendo');
+                },
+                complete: function(response){
+                    // Output AJAX response to the div container
+                    console.log(response);
+                    $("#images_conrainer").html(response.responseText);
+                    
+                    //$('html, body').animate({scrollTop: $(".upload-image-messages").offset().top-100}, 150);
+                    
+                }
+            };  
+            // Submit the form
+            $("#addimage").ajaxForm(options);  
+
+			
+			/*$("#addimage").on("submit",function(event){
+				console.log("submit");
+
+				
+			});*/
+			
+			$(document).on("change","#inputimages",function(event){
+				
+				//$("#addimage").submit();
+				
+				//var postData = $("#addimage").serializeArray();
+				//var formURL = "<?php echo base_url('admin/uploadimages'); ?>"
+				
+				/*$.ajax({
+			        url : formURL,
+			        type: "POST",
+			        data : postData,
+			        success:function(data, textStatus, jqXHR) 
+			        {
+			            console.log(data);
+			            console.log(textStatus);
+			        },
+			        error: function(jqXHR, textStatus, errorThrown) 
+			        {
+			            //if fails    
+			            console.log(jqXHR);
+			            console.log(textStatus);  
+			        }
+    			});	*/
+    			
+    			
+    			
+    			
+
+				
+				
+			});
+			
+			$('#inputimages').submit(function(e) {
+				e.preventDefault();
+				$.ajaxFileUpload({
+					url 			:'<?php echo base_url('admin/uploadimages'); ?>', 
+					secureuri		:false,
+					fileElementId	:'userfile',
+					dataType		: 'json',
+					data			: {
+						'title'				: $('#title').val()
+					},
+					success	: function (data, status)
+					{
+						if(data.status != 'error')
+						{
+							$('#images_conrainer').html('<p>Reloading files...</p>');
+							refresh_files();
+							$('#images_conrainer').val('');
+						}
+						console.log(data.msg);
+					}
+				});
+				return false;
+			});
+			
+			
+			function uoploadfile(target){
+				
+			}
+		</script>
 	</body>
 </html>
